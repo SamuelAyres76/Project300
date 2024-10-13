be9e069e8d786971378ab912cae1766ef56662b3
Alison <112625415+Alison-Doyle@users.noreply.github.com>
2024-10-13 10:26:30 +0100

    :heavy_plus_sign: Added capacitor and scripts to simplify its execution (check READ.ME for them)

---
+++ TimetableApplication/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.timetable.app', // Will need to change this when we get a proper domain
+  appName: 'timetable-application',
+  webDir: 'dist/timetable-application/browser'
+};
+
+export default config;
